8c94bf6dca045b6510a111b77f1aa670d136b330
siyeol97 <leesi2830@naver.com>
2025-01-09 15:04:58 +0900

    💄 Style: update labels to Korean and enhance signup form with additional fields

--- src/app/login/components/LoginForm.tsx
+++ src/app/login/components/LoginForm.tsx
@@ -6,7 +6,7 @@ export default function LoginForm() {
     <section className={styles.container}>
       <form className={styles.form}>
         <div className={styles.input_box}>
-          <label htmlFor='email'>Email</label>
+          <label htmlFor='email'>이메일</label>
           <input
             id='email'
             type='email'
@@ -18,7 +18,7 @@ export default function LoginForm() {
           <p className={styles.error_message}>이메일을 확인해주세요.</p>
         </div>
         <div className={styles.input_box}>
-          <label htmlFor='password'>Password</label>
+          <label htmlFor='password'>비밀번호</label>
           <input
             id='password'
             type='password'
--- src/app/signup/components/SignupForm.tsx
+++ src/app/signup/components/SignupForm.tsx
@@ -6,19 +6,30 @@ export default function SignupForm() {
     <section className={styles.container}>
       <form className={styles.form}>
         <div className={styles.input_box}>
-          <label htmlFor='email'>Email</label>
+          <label htmlFor='email'>이메일</label>
           <input
             id='email'
             type='email'
-            autoFocus
             placeholder='Email *'
             autoComplete='user-name'
+            autoFocus
             className={styles.input}
           />
           <p className={styles.error_message}>이메일을 확인해주세요.</p>
         </div>
         <div className={styles.input_box}>
-          <label htmlFor='password'>Password</label>
+          <label htmlFor='name'>닉네임</label>
+          <input
+            id='name'
+            type='text'
+            placeholder='Name *'
+            autoComplete='user-name'
+            className={styles.input}
+          />
+          <p className={styles.error_message}>이름을 확인해주세요.</p>
+        </div>
+        <div className={styles.input_box}>
+          <label htmlFor='password'>비밀번호</label>
           <input
             id='password'
             type='password'
@@ -27,6 +38,17 @@ export default function SignupForm() {
             className={styles.input}
           />
           <p className={styles.error_message}>비밀번호를 확인해주세요.</p>
+        </div>
+        <div className={styles.input_box}>
+          <label htmlFor='confirm_password'>비밀번호 확인</label>
+          <input
+            id='confirm_password'
+            type='password'
+            placeholder='Confirm Password *'
+            autoComplete='current-password'
+            className={styles.input}
+          />
+          <p className={styles.error_message}>비밀번호가 일치하지 않습니다.</p>
           {/* <p className={styles.error_message}>{errors.password?.message}</p> */}
         </div>
         <AuthButton text='가입하기' />
