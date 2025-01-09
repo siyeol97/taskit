864b4b27ee80eaad612b78c64580954bcca3f150
siyeol97 <leesi2830@naver.com>
2025-01-09 14:19:58 +0900

    💄 Style: update login page layout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import LoginForm from './components/LoginForm';
 import styles from './page.module.scss';
+import Link from 'next/link';
 
 export default function Login() {
   return (
@@ -10,8 +11,13 @@ export default function Login() {
         alt='logo'
         width={144}
         height={36}
+        className={styles.logo}
       />
       <LoginForm />
+      <p className={styles.go_register}>
+        회원이 아니신가요?
+        <Link href={'/signup'}>회원가입</Link>
+      </p>
     </main>
   );
 }
